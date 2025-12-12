d580f795169d9e9d11ee3b3400f094982616d82a
nnk <41167817+nnkar@users.noreply.github.com>
2025-12-12 09:07:02 +0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from logging import StreamHandler
 # Параметры подключения
 username = os.getenv('USERNAME_DATABASE')
 password = os.getenv('PASSWORD_DATABASE')
-host = "mongo-db"  
+host = "mongodb"  
 port = os.getenv('PORT_DATABASE')
 dbname = os.getenv('NAME_DATABASE')
 
